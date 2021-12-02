fa39fa78ae93c2739bee09af52d2ea25e8985d4b
Matt Meyers <matt@mattmeyers.dev>
2021-12-02 17:47:15 -0500

    Convert to typed/racket

--- d1/main.rkt
+++ d1/main.rkt
@@ -1,9 +1,21 @@
-#lang racket
+#lang typed/racket
 
+(provide part-1)
+
+(: read-input (-> String (Listof Integer)))
+(define (read-input filename)
+    (file->list filename))
+
+(: zip-diff (-> (Listof Integer) (Listof Integer)))
 (define (zip-diff l)
-    (map - (cdr l) (reverse(cdr(reverse l)))))
+    (map - (cdr l) (drop-right l 1)))
 
+(: count-positives (-> (Listof Integer) Integer))
+(define (count-positives l)
+    (length (filter positive? l)))
+
+(: part-1 (-> Integer))
 (define (part-1)
-    (length (filter positive? (zip-diff (file->list "input.txt" )))))
+     (count-positives (zip-diff (read-input "input.txt" ))))
 
 (part-1)
