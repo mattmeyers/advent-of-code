9dd300975805965da989af0d242f6698eb129bb9
Matt Meyers <matt@mattmeyers.dev>
2021-12-02 18:07:11 -0500

    Complete day 1, part 2

--- 2021/d1/main.rkt
+++ 2021/d1/main.rkt
@@ -1,10 +1,20 @@
 #lang typed/racket
 
-(provide part-1)
+(provide part-1 part-2)
 
 (: read-input (-> String (Listof Integer)))
 (define (read-input filename)
     (file->list filename))
+
+(: sum (-> (Listof Integer) Integer))
+(define (sum l)
+    (foldr + 0 l))
+
+(: windowify (-> (Listof Integer) (Listof Integer)))
+(define (windowify l)
+    (if (= (length l) 3)
+        (list (foldr + 0 l))
+        (append (list (sum (take l 3))) (windowify (cdr l)))))
 
 (: zip-diff (-> (Listof Integer) (Listof Integer)))
 (define (zip-diff l)
@@ -18,4 +28,8 @@
 (define (part-1)
      (count-positives (zip-diff (read-input "input.txt" ))))
 
+(define (part-2)
+    (count-positives (zip-diff (windowify (read-input "input.txt")))))
+
 (part-1)
+(part-2)
