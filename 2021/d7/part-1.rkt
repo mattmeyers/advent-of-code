cc2ae5478400f0837086367e40f5939401f3409b
Matt Meyers <matt@mattmeyers.dev>
2021-12-07 14:04:48 -0500

    Complete day 7, part 1

---
+++ 2021/d7/part-1.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(define (read-input filename)
+    (file->string filename))
+
+(define (parse-input filename)
+    (map string->number (string-split (read-input filename) ",")))
+
+(define (list-min lst)
+    (foldl min (car lst) lst))
+
+(define (list-max lst)
+    (foldl max (car lst) lst))
+
+(define (possible-values vs)
+    (range (list-min vs) (list-max vs)))
+
+(define (cost n vs)
+    (foldl (lambda (v c) (+ c (abs (- n v)))) 0 vs))
+
+(define (costs positions)
+    (let [(r (possible-values positions))]
+        (map (lambda (n) (cost n positions)) r)))
+
+(list-min (costs (parse-input "input.txt")))
