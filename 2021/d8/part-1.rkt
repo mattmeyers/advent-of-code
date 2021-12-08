f7fcff9d7a56595feb472618ae6f97279a91c9f6
Matt Meyers <matt@mattmeyers.dev>
2021-12-08 00:29:11 -0500

    Complete 2021 Day 8, part 1

---
+++ 2021/d8/part-1.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(define (read-input filename)
+    (file->lines filename))
+
+(define (parse-input lines)
+    (define split-lines (map (lambda (line) (string-split line " | ")) lines))
+    (map 
+        (lambda (line) (string-split (last line)))
+        split-lines))
+
+(define (count-unique-entries lst)
+    (foldl (lambda (v c) (+ c (if (member (string-length v) (list 2 3 4 7)) 1 0))) 0 lst))
+
+(count-unique-entries (flatten(parse-input (read-input "input.txt"))))
