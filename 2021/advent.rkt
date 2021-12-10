66a3217d41e9b6666e74f73bf5b0dccf7a27d1be
Matt Meyers <matt@mattmeyers.dev>
2021-12-10 01:17:09 -0500

    Normalize folder names to sort properly

--- 2021/advent.rkt
+++ 2021/advent.rkt
@@ -1,10 +1,15 @@
 #! /usr/bin/env racket
 #lang racket
 
+(define (normalize-day d)
+    (if (= 1 (string-length d))
+        (format "0~a" d)
+        d))
+
 (define advent
     (command-line
     #:args (day part)
-    (cons day part)))
+    (cons (normalize-day day) part)))
 
 (define code-filename
     (format "./d~a/part-~a.rkt" (car advent) (cdr advent)))
