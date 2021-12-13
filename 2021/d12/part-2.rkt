c295e0b7e4e9c661857332969c3fb99b9b6f7300
Matt Meyers <matt@mattmeyers.dev>
2021-12-12 23:57:54 -0500

    Refactor out common code

--- 2021/d12/part-2.rkt
+++ 2021/d12/part-2.rkt
@@ -1,34 +1,7 @@
 #lang racket
 
 (require racket/runtime-path)
-
-(define (read-input filename)
-    (file->lines filename))
-
-(define (input->adjacency-list lines)
-    (foldl 
-        (lambda (line ht) 
-            (hash-set ht
-                (first line)
-                (append (hash-ref ht (first line) (lambda () empty)) (list (last line)))))
-        #hash() 
-        (split-lines lines)))
-
-(define (split-lines lines) 
-    (filter-not 
-        (lambda (l) (or (string=? "start" (last l)) (string=? "end" (first l))))
-        (apply append 
-            (map (lambda (l) 
-                (list 
-                    (list (first l) (last l))
-                    (list (last l) (first l))))
-                (map (lambda (l) (string-split l "-")) lines)))))
-
-(define (make-adjacency-list filename)
-    (input->adjacency-list (read-input filename)))
-
-(define (small-cave? s)
-    (foldl (lambda (c v) (and v (char-lower-case? c))) #t (string->list s)))
+(require "common.rkt")
 
 (define (update-visited visited v)
     (if (small-cave? v) 
