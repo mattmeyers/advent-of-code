c391ef8e4f70d8412fc206240715f3b0d6369ce9
Matt Meyers <matt@mattmeyers.dev>
2022-12-04 13:09:40 -0500

    Refactor naming and remove unnecessary code

--- 2022/d04/main.hs
+++ 2022/d04/main.hs
@@ -6,46 +6,44 @@ parseInput :: String -> [[[Int]]]
 parseInput contents = map parseLine $ lines contents
 
 parseLine :: String -> [[Int]]
-parseLine = map splitRange . splitLine. pack
+parseLine = map generateRange . splitLine. pack
 
 splitLine :: Text -> [Text]
 splitLine = splitOn (pack ",")
 
-splitRange :: Text -> [Int]
-splitRange t = generateRange (parseInt m) (parseInt n)
+generateRange :: Text -> [Int]
+generateRange t = [(parseInt m)..(parseInt n)]
     where
         [m, n] = splitOn (pack "-") t
 
 parseInt :: Text -> Int
 parseInt t = read $ unpack t
 
-generateRange :: Int -> Int -> [Int]
-generateRange min max = [min..max]
-
-overlaps :: [[Int]] -> Bool
-overlaps ls
+fullyOverlaps :: [[Int]] -> Bool
+fullyOverlaps ls
     | overlapLength > 0 = isFullOverlap overlapLength ls
     | otherwise = False
-    where
-        overlapLength = length $ intersect (head ls) (last ls)
-
-overlaps' :: [[Int]] -> Bool
-overlaps' ls = overlapLength > 0
     where
         overlapLength = length $ intersect (head ls) (last ls)
 
 isFullOverlap :: Int -> [[Int]] -> Bool
 isFullOverlap ol = any (\l -> ol == length l)
 
+overlaps :: [[Int]] -> Bool
+overlaps ls = overlapLength > 0
+    where
+        overlapLength = length $ intersect (head ls) (last ls)
+
+
 intersect :: [Int] -> [Int] -> [Int]
 intersect [] = const []
 intersect xs = filter (`elem` xs)
 
 solve :: [[[Int]]] -> Int
-solve ls = length $ filter overlaps ls
+solve ls = length $ filter fullyOverlaps ls
 
 solve' :: [[[Int]]] -> Int
-solve' ls = length $ filter overlaps' ls
+solve' ls = length $ filter overlaps ls
 
 main :: IO ()
 main = do
