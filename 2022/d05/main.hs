bf1e428e1ace08b211c074976d9257858134de0a
Matt Meyers <matt@mattmeyers.dev>
2022-12-06 22:57:42 -0500

    Complete 2022 day 5 part 2

--- 2022/d05/main.hs
+++ 2022/d05/main.hs
@@ -16,7 +16,7 @@ parseInput s = (parseInputState inputState, parseInputMoves moves)
         moves = unpack $ last parts
 
 parseInputState :: String -> State
-parseInputState s = fromList . fmap parseLine . transpose . tail . reverse . map splitLine $ lines s
+parseInputState s = fromList . fmap (reverse . parseLine) . transpose . tail . reverse . map splitLine $ lines s
 
 splitLine :: String -> [String]
 splitLine s = map (take 3) $ chunk 4 (s ++ " ")
@@ -47,12 +47,18 @@ parseInt = read
 run :: (State, [Move]) -> State
 run (state, moves) = foldl applyMove state moves
 
+run' :: (State, [Move]) -> State
+run' (state, moves) = foldl applyMove' state moves
+
 applyMove :: State -> Move -> State
 applyMove s (0, src, dst) = s
-applyMove s (n, src, dst) = applyMove (moveCrate s src dst) (n-1, src, dst)
+applyMove s (n, src, dst) = applyMove (moveCrate s 1 src dst) (n-1, src, dst)
 
-moveCrate :: State -> Int -> Int -> State
-moveCrate s src dst = removeCrate (src-1) $ pushCrate s (dst-1) (last $ grabStack (src-1) s)
+applyMove' :: State -> Move -> State
+applyMove' s (n, src, dst) = moveCrate s n src dst
+
+moveCrate :: State -> Int -> Int -> Int -> State
+moveCrate s nCrates src dst = removeCrates (src-1) nCrates $ pushCrates s (dst-1) (take nCrates $ grabStack (src-1) s)
 
 grabStack :: Int -> State -> [String]
 grabStack n s = grabValue $ lookup n s
@@ -61,20 +67,21 @@ grabValue :: Maybe [String] -> [String]
 grabValue Nothing = []
 grabValue (Just v) = v
 
-pushCrate :: State -> Int -> String -> State
-pushCrate s n val = update n (grabStack n s ++ [val]) s
+pushCrates :: State -> Int -> [String] -> State
+pushCrates s n val = update n (val ++ grabStack n s) s
 
-removeCrate :: Int -> State -> State
-removeCrate n s = update n (init' $ grabStack n s) s
+removeCrates :: Int -> Int -> State -> State
+removeCrates stackNum nCrates s = update stackNum (drop nCrates $ grabStack stackNum s) s
 
 init' :: [a] -> [a]
 init' [] = []
 init' xs = init xs
 
 topCrates :: State -> String
-topCrates = foldl (\acc v -> acc ++ last v ) ""
+topCrates = foldl (\acc v -> acc ++ head v ) ""
 
 main :: IO ()
 main = do
     contents <- readFile "input.txt"
     print . topCrates . run $ parseInput contents
+    print . topCrates . run' $ parseInput contents
