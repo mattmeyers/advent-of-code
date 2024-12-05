e223f837370b88c83c8864e8d1bfa91613ad5bf0
Matt Meyers <matt@mattmeyers.dev>
2024-12-04 21:18:31 -0500

    [2024] Add new day script

---
+++ 2024/new.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+set -e
+
+day="$1"
+if [ $# = 0 ]; then
+    echo "Day number required"
+    exit 1
+elif [[ $day -le 9 ]]; then
+    day="0$day"
+fi
+
+directory="d$day"
+
+mkdir "$directory"
+touch "$directory/sample.txt"
+touch "$directory/input.txt"
+cat > "$directory/index.php"  <<CODE
+<?php
+
+/** @return array<int, string> */
+function parseInput(string \$filename): array
+{
+    \$lines = [];
+    foreach (file(\$filename) as \$line) {
+        \$lines[] = trim(\$line);
+    }
+
+    return \$lines;
+}
+
+/** @return array<int, string> */
+function part1(array \$values): int
+{
+    return 0;
+}
+
+/** @return array<int, string> */
+function part2(array \$values): int
+{
+    return 0;
+}
+
+\$sampleValues = parseInput("sample.txt");
+\$inputValues = parseInput("input.txt");
+
+\$p1Sample = part1(\$sampleValues);
+\$p1Input = part1(\$inputValues);
+assert(\$p1Sample == 0);
+assert(true || \$p1Input == 0);
+
+\$p2Sample = part2(\$sampleValues);
+\$p2Input = part2(\$inputValues);
+assert(true || \$p2Sample == 0);
+assert(true || \$p2Input == 0);
+
+printf("PART 1:\n");
+printf("    Sample: %d\n", \$p1Sample);
+printf("    Input:  %d\n", \$p1Input);
+
+printf("PART 2:\n");
+printf("    Sample: %d\n", \$p2Sample);
+printf("    Input:  %d\n", \$p2Input);
+CODE
+
